a617c7caf029906fe7bdc55603bfcf14721aa9d3
Dylan Berghuis <47518792+dylaninbcky@users.noreply.github.com>
2019-07-01 20:57:11 +0200

    Added JSON support

--- Poshtify/Public/Connect-Poshtify.ps1
+++ Poshtify/Public/Connect-Poshtify.ps1
@@ -1,10 +1,14 @@
 Function Connect-PoshTify {
     [Cmdletbinding()]
     param (
-        [parameter(Mandatory,Position = 0)]
+        [parameter(Mandatory, Position = 0)]
         $ClientID,
-        [parameter(Mandatory,Position = 1)]
-        $ClientSecret
+        [parameter(Mandatory, Position = 1)]
+        $ClientSecret,
+        [parameter(HelpMessage = "for JSON output ## TESTING")]
+        [switch]$JSON,
+        [parameter(HelpMessage = "Output path, ONLY IF JSON SWITCH IS ACTIVATED")]
+        $outputpath
     )
     BEGIN {
         $URL = New-PoshtifyURL -Client_ID $ClientID
@@ -19,15 +23,26 @@ Function Connect-PoshTify {
         }
         if ($AUTHCODE) {
             try {
-            $Token = Get-Poshtifyrefreshtoken -AuthCode $AUTHCODE -Client_ID $ClientID -Client_Secret $ClientSecret
-            Set-Variable -Name AccessCode -Value $Token -Scope Global
-            Write-Output 'Global variable created, check by running $global:Accesscode'
+                $Token = Get-Poshtifyrefreshtoken -AuthCode $AUTHCODE -Client_ID $ClientID -Client_Secret $ClientSecret
+                if (!$JSON) {
+                    $token | Add-Member -NotePropertyName 'Client_ID' -NotePropertyValue $ClientID
+                    $token | Add-Member -NotePropertyName 'Client_Secret' -NotePropertyValue $ClientSecret
+                    Set-Variable -Name AccessCode -Value $Token -Scope Global
+                    Write-Output 'Global variable created, check by running $global:Accesscode'
+                }
+                else {
+                    $token | Add-Member -NotePropertyName 'Client_ID' -NotePropertyValue $ClientID
+                    $token | Add-Member -NotePropertyName 'Client_Secret' -NotePropertyValue $ClientSecret
+                    ConvertTo-Json -InputObject $token | Out-File "$outputpath\session.json"
+                    Write-Output 'JSON created!'
+                }
+            
             }
-            catch{
+            catch {
                 Throw "Cannot open login page" 
             }
         }
-        else{
+        else {
             Throw "Auth code is empty, did you provide clientid & secret?"
         }
     }
